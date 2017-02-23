0264ecb39796cb25a0e47fae7efda7121ff24347
joncjing <116384196@qq.com>
2017-02-23 17:54:01 +0800

    Add Redis

---
+++ src/Redis/Redis.java
@@ -0,0 +1,44 @@
+package Redis;
+
+
+import redis.clients.jedis.Jedis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hcb on 2017/2/23.
+ */
+public class Redis {
+
+    static Jedis jedis = new Jedis("localhost");
+
+    public static void main(String[] args){
+        setAndgetList();
+    }
+
+    public static void CheckStatus(){
+        System.out.println("Redis Connection Status is:");
+        System.out.println("Status is: " + jedis.ping());
+    }
+
+    /*
+        Key-Value
+     */
+    public static void setAndgetValue(String value){
+        jedis.set("name",value);
+        String returnValue = jedis.get("name");
+        System.out.println(returnValue);
+    }
+
+    public static void setAndgetList(){
+//        jedis.lpush("myList","joncjing");
+//        jedis.lpush("myList","jingjing");
+
+        List<String> myList = new ArrayList<String>();
+        myList = jedis.lrange("myList",0,2);
+        for (int i=0;i<myList.size();i++){
+            System.out.println(myList.get(i));
+        }
+    }
+}
