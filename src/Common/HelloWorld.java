ac79d72e32181bd6cba0b3ce6dcea74ce69d2c55
joncjing <116384196@qq.com>
2017-02-23 15:40:17 +0800

    add a comment

--- src/Common/HelloWorld.java
+++ src/Common/HelloWorld.java
@@ -4,7 +4,7 @@ package Common;
  * Created by hcb on 2017/2/23.
  */
 public class HelloWorld {
-
+    // add a comment
     public static void main(String[] args){
         System.out.println("helloWorld");
     }
