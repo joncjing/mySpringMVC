b5f01a1276b5b5d63f20b50d1415972abb601b72
joncjing <116384196@qq.com>
2017-02-23 15:33:49 +0800

    add a new spring mvc project

---
+++ src/Common/HelloWorld.java
@@ -0,0 +1,11 @@
+package Common;
+
+/**
+ * Created by hcb on 2017/2/23.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("helloWorld");
+    }
+}
