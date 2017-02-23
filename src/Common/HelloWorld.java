c709672f2127c51a6b53d722a5355b6209e32952
joncjing <116384196@qq.com>
2017-02-23 16:34:42 +0800

    Add IO Package

--- src/Common/HelloWorld.java
+++ src/Common/HelloWorld.java
@@ -6,7 +6,6 @@ package Common;
 public class HelloWorld {
     // add a comment
     // This is a test java file
-    // edit by others
     public static void main(String[] args){
         System.out.println("helloWorld");
     }
